dd46cc4ba096727182e519e8b34d3242a43247c0
Troshin-Egor-py <145129273+Troshin-Egor-py@users.noreply.github.com>
2024-06-16 00:32:03 +0300

    add_font_and_text

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ import pygame
 import random
 
 
+pygame.font.init()
+
+
 # Constants
 # Sizes
 SIZE_BLOCK = 50
-SIZE_FIELD_IN_BLOCKS = (8, 8)
+SIZE_FIELD_IN_BLOCKS = (4, 4)
 SIZE_SPACE = 10
 SIZE_WINDOW = (
-    (SIZE_BLOCK + SIZE_SPACE) * SIZE_FIELD_IN_BLOCKS[0] - SIZE_SPACE,
-    (SIZE_BLOCK + SIZE_SPACE) * SIZE_FIELD_IN_BLOCKS[1] - SIZE_SPACE
+    (SIZE_BLOCK + SIZE_SPACE) * SIZE_FIELD_IN_BLOCKS[0] + SIZE_SPACE,
+    (SIZE_BLOCK + SIZE_SPACE) * SIZE_FIELD_IN_BLOCKS[1] + SIZE_SPACE
 )
 
 # Colors
@@ -17,14 +20,19 @@ COLOR_BACKGROUND = (255, 255, 255)
 COLOR_BLOCK = (0, 255, 0)
 COLOR_APPLE = (255, 0, 0)
 COLOR_FONT = (0, 182, 63)
+COLOR_WIN_TEXT = (0, 185, 69)
+COLOR_OVER_TEXT = (255, 116, 0)
+
 
 # Other
 W = pygame.display.set_mode((SIZE_WINDOW[0], SIZE_WINDOW[1]))
 C = pygame.time.Clock()
-# F = pygame.font.SysFont('Stick', 60) 
-# F_SHADOW = pygame.font.SysFont('Stick', 65) 
+WIN_TEXT = 'ПОБЕДА!!!'
+OVER_TEXT = {'edge': 'ПОКА :(', 'tail': 'АЙ! НЕВКУСНО!'}
 FPS = 60
 TIME_DELAY_MOVE = 300
+FONT_FILE = 'black-pixel.ttf'
+
 
 t = pygame.time.get_ticks()
 snake_vector = (1, 0)
@@ -54,18 +62,18 @@ class Head(Block):
         self.space_vector = (0, 0)
         all_cells[from_pos_to_num(self.rect.x, self.rect.y)].disactivate()
     def check_out(self):
-        if self.rect.x > SIZE_WINDOW[0] or self.rect.x < 0 or self.rect.y > SIZE_WINDOW[1] or self.rect.y < 0:
-            game_over()
+        if self.rect.x >= SIZE_WINDOW[0] or self.rect.x < 0 or self.rect.y >= SIZE_WINDOW[1] or self.rect.y < 0:
+            game_over('edge')
     def check_collide(self):
         try:
-            all_cells[from_pos_to_num(self.rect.y, self.rect.x)].disactivate()
+            all_cells[from_pos_to_num(self.rect.x, self.rect.y)].disactivate()
         except ValueError:
-            game_over()
+            game_over('tail')
 class Snake():
     def __init__(self, *blocks) -> None:
         self.blocks = list(blocks)
     def move(self) -> None:
-        all_cells[from_pos_to_num(self.blocks[-1].rect.y, self.blocks[-1].rect.x)].activate()
+        all_cells[from_pos_to_num(self.blocks[-1].rect.x, self.blocks[-1].rect.y)].activate()
         for i in self.blocks:
             i.move()
         self.blocks[0].check_out()
@@ -84,7 +92,7 @@ class Snake():
         self.blocks.append(Block(last_coordinate[0], last_coordinate[1]))
         self.blocks[-1].vector = self.blocks[-2].vector
         self.blocks[-1].space_vector = self.blocks[-2].vector
-        all_cells[from_pos_to_num(self.blocks[-1].rect.y, self.blocks[-1].rect.x)].disactivate()
+        all_cells[from_pos_to_num(self.blocks[-1].rect.x, self.blocks[-1].rect.y)].disactivate()
 class Apple():
     def __init__(self) -> None:
         self.rect = pygame.Rect(0, 0, SIZE_BLOCK, SIZE_BLOCK)
@@ -97,7 +105,7 @@ class Apple():
             self.replace_pos()
     def replace_pos(self):
         pos = random.choice(null_cells)
-        self.rect.topleft = (pos[0] * (SIZE_BLOCK + SIZE_SPACE), pos[1] * (SIZE_BLOCK + SIZE_SPACE))
+        self.rect.topleft = (pos[0] * (SIZE_BLOCK + SIZE_SPACE) + SIZE_SPACE, pos[1] * (SIZE_BLOCK + SIZE_SPACE) + SIZE_SPACE)
      
     def draw(self) -> None:
         pygame.draw.rect(W, COLOR_APPLE, self.rect)
@@ -135,16 +143,33 @@ def events() -> None:
                 if snake_vector != (1, 0):
                     temporary_vector = (-1, 0)
 def from_pos_to_num(x, y) -> int:
+    x -= SIZE_SPACE
+    y -= SIZE_SPACE
     x /= SIZE_BLOCK + SIZE_SPACE
     y /= SIZE_BLOCK + SIZE_SPACE
     return int(y * SIZE_FIELD_IN_BLOCKS[0] + x)
 def win():
+    f = pygame.font.Font(FONT_FILE, 20)
+    test_text = f.render(WIN_TEXT, True, (0, 0, 0))
+    F = pygame.font.Font(FONT_FILE, int(SIZE_WINDOW[0] * 0.8 / test_text.get_width() * test_text.get_height()))
+    text = F.render(WIN_TEXT, True, COLOR_WIN_TEXT)
+    del f, test_text
+
     snake.draw()
+    W.blit(text, ((SIZE_WINDOW[0] - text.get_width()) / 2, (SIZE_WINDOW[1] - text.get_height()) / 2))
     pygame.display.flip()
     delay_exit()
-def game_over():
+def game_over(type_over):
+    text_over = OVER_TEXT[type_over]
+    f = pygame.font.Font(FONT_FILE, 20)
+    test_text = f.render(text_over, True, (0, 0, 0))
+    F = pygame.font.Font(FONT_FILE, int(SIZE_WINDOW[0] * 0.8 / test_text.get_width() * test_text.get_height()))
+    text = F.render(text_over, True, COLOR_OVER_TEXT)
+    del f, test_text
+
     snake.draw()
     apple.draw()
+    W.blit(text, ((SIZE_WINDOW[0] - text.get_width()) / 2, (SIZE_WINDOW[1] - text.get_height()) / 2))
     pygame.display.flip()
     delay_exit()
 def delay_exit():
@@ -157,9 +182,9 @@ def delay_exit():
 all_cells = []
 null_cells = []
 for i in range(SIZE_FIELD_IN_BLOCKS[0] * SIZE_FIELD_IN_BLOCKS[1]):
-    all_cells.append(Null_cell(i // SIZE_FIELD_IN_BLOCKS[0], i % SIZE_FIELD_IN_BLOCKS[0]))
+    all_cells.append(Null_cell(i % SIZE_FIELD_IN_BLOCKS[0], i // SIZE_FIELD_IN_BLOCKS[0]))
 
-snake = Snake(Head(0, 0))
+snake = Snake(Head(SIZE_SPACE, SIZE_SPACE))
 apple = Apple()
 
 
